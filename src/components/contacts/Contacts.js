f4459a7e1335f31d429d6d20d624fdd308cf4405
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-10 23:29:45 +0100

    contact

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/Contact.css";
 
 const Contacts = () => {
+  const [email, setEmail] = useState("");
+
+  const [check, setCheck] = useState(false);
+
+  const validate = () => {
+    return email.length && check;
+  };
+
+  const handleCheck = () => {
+    setCheck(!check);
+  };
+
+  const submit = () => {
+    alert("Enviado");
+  };
   return (
     <>
-      <h5>CONTACT</h5>
       <div className="contact_box">
-        <div class="grid-container">
-          <div class="grid-child purple">
-            email
+        <div className="grid-container">
+          <div className="grid-child purple">
+            <h5>
+              <b>CONTACT</b>
+            </h5>
+            <label>Email</label>
+            <br />
+            <input
+              className="input"
+              type="email"
+              placeholder="Introduce tu email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <br />
+            <input type="checkbox" value={check} onChange={handleCheck} />
+            <label className="label_privacidad">
+              Al suscribirte, aceptas{" "}
+              <b>
+                <font color="blue">política de privacidad</font>
+              </b>
+            </label>
+            <br />
+            <button onClick={submit} disabled={!validate()} className="submit">
+              <b>ENVIAR</b>
+            </button>
           </div>
-
-          <div class="grid-child green">
-            SOCIAL
+          <div className="grid-child green">
+            <h5>
+              <b>SOCIAL</b>
+            </h5>
           </div>
         </div>
       </div>
