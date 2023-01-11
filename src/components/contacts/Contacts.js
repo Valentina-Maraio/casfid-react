523162d72f92e155a98573e46b0d76fdb797ab27
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-11 15:29:42 +0100

    home done

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -7,6 +7,7 @@ import { ImFacebook } from "react-icons/im";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Contacts = () => {
   const [email, setEmail] = useState("");
@@ -22,41 +23,51 @@ const Contacts = () => {
   };
 
   const submit = () => {
-    alert("Enviado");
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Correo enviado correctamente",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
   return (
     <>
       <Container className="d-flex justify-content-center">
         <Row>
           <Col md="auto">
-              <h5>
-                <b>CONTACT</b>
-              </h5>
-              <label>Email</label>
-              <br />
-              <input
-                className="input"
-                type="email"
-                placeholder="Introduce tu email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <br />
-              <input type="checkbox" value={check} onChange={handleCheck} />
-              <label className="label_privacidad">
-                Al suscribirte, aceptas{" "}
-                <b>
-                  <font color="blue">política de privacidad</font>
-                </b>
-              </label>
-              <br />
-              <button
-                onClick={submit}
-                disabled={!validate()}
-                className="submit"
-              >
-                <b>ENVIAR</b>
-              </button>
+            <h5>
+              <b>CONTACT</b>
+            </h5>
+            <label>Email</label>
+            <br />
+            <input
+              className="input"
+              type="email"
+              placeholder="Introduce tu email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <br />
+            <input type="checkbox" value={check} onChange={handleCheck} />
+            <label className="label_privacidad">
+              Al suscribirte, aceptas{" "}
+              <b>
+                <font color="blue">política de privacidad</font>
+              </b>
+            </label>
+            <br />
+            <button
+              onClick={() => {
+                submit();
+                setEmail("");
+                setCheck(false);
+              }}
+              disabled={!validate()}
+              className="submit"
+            >
+              <b>ENVIAR</b>
+            </button>
           </Col>
           <Col md="auto">
             <div className="justify-content-center">
@@ -65,17 +76,45 @@ const Contacts = () => {
               </h5>
               <div className="social_box">
                 <ul className="social_box">
-                  <li className="facebook">
-                    <ImFacebook size="25px"/>
+                  <li>
+                    <a
+                      className="linkedin"
+                      href="https://www.linkedin.com/company/casfid"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaLinkedinIn size="25px" />
+                    </a>
                   </li>
-                  <li className="instagram">
-                    <AiOutlineInstagram size="25px"/>
+                  <li>
+                    <a
+                      className="facebook"
+                      href="https://www.facebook.com/casfid.es"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <ImFacebook size="25px" />
+                    </a>
                   </li>
-                  <li className="linkedin">
-                    <FaLinkedinIn size="25px"/>
+                  <li>
+                    <a
+                      className="instagram"
+                      href="https://www.instagram.com/casfid/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <AiOutlineInstagram size="25px" />
+                    </a>
                   </li>
-                  <li className="youtube">
-                    <FaYoutube size="25px"/>
+                  <li>
+                    <a
+                      className="youtube"
+                      href="https://www.youtube.com/channel/UCb2wG4iXm-mQZiR9A2SjQqw"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaYoutube size="25px" />
+                    </a>
                   </li>
                 </ul>
               </div>
@@ -83,7 +122,7 @@ const Contacts = () => {
           </Col>
         </Row>
       </Container>
-      <br/>
+      <br />
     </>
   );
 };
